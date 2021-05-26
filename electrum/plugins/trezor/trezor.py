51f952440c38f0096670de0a485841add4bac11f
kralverde <electrum.raven@gmail.com>
2021-05-26 19:39:21 -0400

    Trezor tweaks

--- electrum/plugins/trezor/trezor.py
+++ electrum/plugins/trezor/trezor.py
@@ -117,7 +117,7 @@ class TrezorPlugin(HW_PluginBase):
     libraries_URL = 'https://pypi.org/project/trezor/'
     minimum_firmware = (1, 6, 0)
     keystore_class = TrezorKeyStore
-    minimum_library = (0, 12, 0)
+    minimum_library = (0, 11, 0)
     maximum_library = (0, 13)
     SUPPORTED_XTYPES = ('standard', 'p2wpkh-p2sh', 'p2wpkh', 'p2wsh-p2sh', 'p2wsh')
     DEVICE_IDS = (TREZOR_PRODUCT_KEY,)
@@ -206,7 +206,7 @@ class TrezorPlugin(HW_PluginBase):
         return client
 
     def get_coin_name(self):
-        return "Testnet" if constants.net.TESTNET else "Ravencoin"
+        return "Ravencoin Testnet" if constants.net.TESTNET else "Ravencoin"
 
     def initialize_device(self, device_id, wizard, handler):
         # Initialization method
